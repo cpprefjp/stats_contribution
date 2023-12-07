c9a76b1c14d034c29a21fdb6c25bf2e6613b41bc
Akira Takahashi <faithandbrave@gmail.com>
2023-12-07 18:33:25 +0900

    辞退者を追加

--- run_2023.sh
+++ run_2023.sh
@@ -1,1 +1,1 @@
-sh run.sh --exclude-users faithandbrave,yumetodo,toru-fukaya,onihusube,sakujiro-inagaki,yohhoy
+sh run.sh --exclude-users faithandbrave,yumetodo,toru-fukaya,onihusube,sakujiro-inagaki,yohhoy,tshino
